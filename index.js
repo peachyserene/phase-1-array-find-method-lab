e3b634b4d53bf9dbd9767c91bb305b9aabcae5de
Georgia Burnside <burnsidegeorgia@gmail.com>
2022-04-12 20:47:47 -0400

    commit tree 777eaf97e11a5af036a349f79296f6127402d6dd

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function superbowlWin (array) {
+    let lineFound = array.find(element => element.result === "W")
+    return lineFound ? lineFound.year : undefined
+  }
+
+  //original if else before using ternary 
+/*function superbowlWin (array) {
+    let lineFound = array.find(element => element.result === "W")
+    if (lineFound) {
+        return lineFound.year;
+      }
+        else{
+            return undefined;
+        }   
+      }*/
